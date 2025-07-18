0b5e411e0092a6d998a5d9e5904666c4ffd1b2b5
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-18 13:08:12 +0200

    aggiorna gestione della lista articoli in App.jsx; correggi il rendering e la logica di invio del modulo

--- src/App.jsx
+++ src/App.jsx
@@ -2,24 +2,30 @@ import { useState } from 'react'
 
 function App() {
   const articles = ["Lorem1", "Lorem2", "Lorem3", "Lorem4", "Lorem5", "Lorem6"]
-  const [list, setList] = useState("")
+  const [list, setList] = useState(articles)
+  const [item, setItem] = useState("")
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setList([...list, item])
 
 
-  function handleSubmit(event) {
-    event.preventDefault();
   }
   return (
     <>
-      <form onSubmit={handleSubmit}>
-        <div className="container">
-          <ul>
-            {articles.map((article) => (
-              <li key={id}>{article}</li>
-            ))}
-          </ul>
-          <input type="text" value={list} onChange={e => { setList(e.target.value) }} />
-        </div>
-      </form >
+      <div className="container">
+        <ul>
+          {list.map((article) => (
+            <li>{article}</li>
+          ))}
+        </ul>
+        <form onSubmit={handleSubmit}>
+          <input type="text" value={item} onChange={e => { setItem(e.target.value) }} />
+          <button type="submit" className="btn btn-primary ms-2">
+            <i className="bi bi-plus"></i>
+          </button>
+        </form >
+      </div>
     </>
   )
 }
