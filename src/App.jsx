529ca27dec8b4a37310fa78bd856e1127f95f40f
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-18 16:59:57 +0200

    aggiorna la gestione della lista articoli in App.jsx; migliora il rendering e la logica di invio del modulo

--- src/App.jsx
+++ src/App.jsx
@@ -2,29 +2,43 @@ import { useState } from 'react'
 
 function App() {
   const articles = []
+
   const [list, setList] = useState(articles)
   const [item, setItem] = useState("")
 
+
   function handleSubmit(e) {
     e.preventDefault();
-    setList([...list, item])
+    setList([
+      ...list,
+      {
+        id: crypto.randomUUID(),
+        title: item,
+      }
+    ])
     setItem("");
+  }
+  function handleDelete(id) {
+    setList(prev => prev.filter(post => post.id !== id))
+    /* setList(prev => prev.filter(post => post.id !== id))
+    setList(prev => prev.filter(post => post.id !== id)) */
   }
   return (
     <>
-      <div className="container">
-        <ul>
-          {list.map((article, index) => (
-            <li key={index}>
-              {article}
-              <button type="button" className="btn ">
-                <i className="bi bi-trash3"></i></button>
+      <div className="container text-center">
+        <ul className="list-unstyled">
+          <h1 className="bg-secondary-subtle">Todolist</h1>
+          {list.map((article) => (
+            <li key={article.id}>
+              {article.title}
+              <button type="button" className="btn " onClick={() => handleDelete(article.id)}>
+                <i className="btn btn-danger bi bi-trash3"></i></button>
             </li>
           ))}
         </ul>
         <form onSubmit={handleSubmit}>
           <input type="text" value={item} onChange={e => { setItem(e.target.value) }} />
-          <button type="submit" className="btn btn-primary ms-2">
+          <button type="submit" className="btn btn-success ms-2">
             <i className="bi bi-plus"></i>
           </button>
         </form >
