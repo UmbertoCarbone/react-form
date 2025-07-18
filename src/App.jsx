96742cee43be73be85ef649f059a485f077770f7
UmbertoCarbone <umberto.carb.dev@gmail.com>
2025-07-18 12:36:25 +0200

    aggiungi bootstrap e bootstrap-icons come dipendenze; rimuovi App.css e aggiorna App.jsx per gestire la lista degli articoli

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const articles = ["Lorem1", "Lorem2", "Lorem3", "Lorem4", "Lorem5", "Lorem6"]
+  const [list, setList] = useState("")
 
+
+  function handleSubmit(event) {
+    event.preventDefault();
+  }
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <form onSubmit={handleSubmit}>
+        <div className="container">
+          <ul>
+            {articles.map((article) => (
+              <li key={id}>{article}</li>
+            ))}
+          </ul>
+          <input type="text" value={list} onChange={e => { setList(e.target.value) }} />
+        </div>
+      </form >
     </>
   )
 }
